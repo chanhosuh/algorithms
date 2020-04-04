2bc65ea1da768155b50493c13b11bf62c6820f1f
Chan-Ho Suh <chanhosuh@gmail.com>
2020-04-04 14:07:22 -0400

    comments on complexity of quick union

--- union_find/quick_union.py
+++ union_find/quick_union.py
@@ -19,6 +19,9 @@ class QuickUnion(UnionFind):
            component trees for a and b
         2. link the root id for a to root id for b 
         3. decrement count
+        
+        Runtime complexity of this algorithm is
+        2 * "find" complexity + constant.
         """
         root_id_a = self.find(a)
         root_id_b = self.find(b)
@@ -41,6 +44,10 @@ class QuickUnion(UnionFind):
            you reach a self-link, i.e. an item
            linking to itself
         2. return self-link
+        
+        Runtime complexity is O(log k) where k is
+        the number of items in the component tree
+        for a.
         """
         while a != self.ids[a]:
             a = self.ids[a]
