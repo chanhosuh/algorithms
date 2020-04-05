806856dd7f73084510461bbe4fd47b8133d150b6
Chan-Ho Suh <chanhosuh@gmail.com>
2020-04-04 22:58:41 -0400

    start articulation point

---
+++ graphs/depth_first_search/articulation_point.py
@@ -0,0 +1,46 @@
+"""
+Given a graph, find its articulation points, e.g. a point
+whose removal increases the number of components of the
+graph.
+"""
+from graphs.graph import create_graph
+
+
+# pylint: disable=redefined-outer-name
+class ArticulationPoints:
+    def __init__(self, graph):
+        """
+        given adjacency representation of a graph
+
+        returns list of articulation points
+        """
+        # current step of DFS
+        self.step = 0
+        # step number when visited
+        # -1 means not visited
+        self.visit_steps = [-1] * graph.num_vertices
+        # earliest vertex in DFS a vertex is connected to
+        self.earliest = [-1] * graph.num_vertices
+
+    def depth_first_search(self, graph, u, v):
+        """
+        Through DFS, an articulation point v is found
+        precisely when one of two conditions hold:
+        
+        1. v is a root vertex of the DFS tree and
+           has more than one successor
+        2. v is an interior vertex of the DFS tree
+           and has a successor w such that:
+              
+              earliest(w) >= visit_step(v)
+        """
+
+
+if __name__ == "__main__":
+    """
+    Expected result:
+    
+
+    """
+    graph = create_graph("graphs/data/tinyG.txt")
+    ap = ArticulationPoints(graph)
