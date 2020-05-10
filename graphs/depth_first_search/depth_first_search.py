0cf4580489aef76bc04e612a950233ef49129213
Chan-Ho Suh <chanhosuh@gmail.com>
2020-05-10 15:55:00 -0400

    minor cleanup of DFS

--- graphs/depth_first_search/depth_first_search.py
+++ graphs/depth_first_search/depth_first_search.py
@@ -1,13 +1,18 @@
 class DepthFirstSearch(object):
     def __init__(self, graph):
         self.visited = [False] * graph.num_vertices
-        self.graph = graph
+        
+        for v in graph.vertices:
+            if not self.visited[v]:
+                continue
+            self.search(v)
 
     def search(self, vertex):
         self.visited[vertex] = True
         for v in self.graph.adj[vertex]:
-            if not self.visited[v]:
-                self.search(v)
+            if self.visited[v]:
+                continue
+            self.search(v)
 
 
 DFS = DepthFirstSearch
